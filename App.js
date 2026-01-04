a24597f4fbb88772b6c3cc53b5880992bbab3c9c
Nandhini3115 <jnandhini3115@gmail.com>
2026-01-04 13:42:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeTable from "./EmployeeTable";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Employee Management System</h2>
+      <EmployeeTable />
+    </div>
+  );
+}
+
+export default App;
